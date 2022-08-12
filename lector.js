b82e9b64529e286ec2a5e6b61168136f038a2e41
app-moriah <111099497+app-moriah@users.noreply.github.com>
2022-08-12 15:58:27 -0600

    initial commit
    dos lectores y agregaciones

--- lector.js
+++ lector.js
@@ -4,17 +4,22 @@ function loadLector(){
 		const $resultados = document.querySelector("#resultado");
 		Quagga.init({
 			inputStream: {
-				 constraints: {
-				 	width: 1920,
-				 	height: 1080,
-				 },
 				name: "Live",
 				type: "LiveStream",
-				target: document.querySelector('#contenedor'), // Pasar el elemento del DOM
+				constraints: {
+					  width: 640,
+					  height: 480,
+					  facing: "environment"
+				  }
 			},
-			decoder: {
-				readers: ["ean_reader"]
-				// readers: ["upc_reader"]
+			locator: {
+			  patchSize: "medium",
+			  halfSample: true
+			},
+			numOfWorkers: 4,
+			locate: true,
+			decoder : {
+				readers: ["upc_reader", "ean_reader"]
 			}
 		}, function (err) {
 			if (err) {
@@ -28,22 +33,22 @@ function loadLector(){
 		Quagga.onDetected((data) => {
 			$resultados.textContent = data.codeResult.code;
 
-			const datos = data.codeResult.code;
-			// const result = datos.indexOf(0,0);       
+			// const datos = data.codeResult.code;
+			// // const result = datos.indexOf(0,0);       
 	
-			if(datos.indexOf(0)===0){
-				// alert("0 esta adelante")
-				  const str = data.codeResult.code;
-					const newStr = str.slice(1)
-					document.getElementById("resultado").value="";
-					document.getElementById("resultado").value=newStr;
-			}else{
-				document.getElementById("resultado").value="";
-				document.getElementById("resultado").value=data.codeResult.code;
-			}
+			// if(datos.indexOf(0)===0){
+			// 	// alert("0 esta adelante")
+			// 	  const str = data.codeResult.code;
+			// 		const newStr = str.slice(1)
+			// 		document.getElementById("resultado").value="";
+			// 		document.getElementById("resultado").value=newStr;
+			// }else{
+			// 	document.getElementById("resultado").value="";
+			// 	document.getElementById("resultado").value=data.codeResult.code;
+			// }
 
 
-			// document.getElementById("resultado").value = data.codeResult.code;
+			document.getElementById("resultado").value = data.codeResult.code;
 			sound.play();	
 			// setTimeout(clickear, 500);
 			// function clickear(){
